36993abdc0edfbdf6cdfbd878839ff2a1b02ba4f
Kier Davis <kierdavis@gmail.com>
2016-11-21 09:19:02 +0000

    Revert "Change 'state' from an enum to plain logic type"
    This reverts commit 729fcd31134e432fd70335ed421db154173ba3bf.

--- src/multiplier/multiplier_controller.sv
+++ src/multiplier/multiplier_controller.sv
@@ -15,11 +15,6 @@ module multiplier_controller(
   counter_do_decrement
 );
 
-  localparam STATE_BITS = 2;
-  localparam IDLE = 2'd0;
-  localparam WORKING = 2'd1;
-  localparam DONE = 2'd2;
-
   //// Ports ////
   input logic clock;
   input logic reset_n;
@@ -32,7 +27,7 @@ module multiplier_controller(
   output logic counter_do_decrement;
 
   //// Internal nodes ////
-  logic [STATE_BITS-1:0] state, next_state;
+  enum {IDLE, WORKING, DONE} state, next_state;
 
   //// Register clock logic ////
   always_ff @(posedge clock or negedge reset_n) begin
@@ -82,11 +77,6 @@ module multiplier_controller(
       DONE: begin
         // Assert the ready signal and stay in this state forever.
         ready = 1'd1;
-      end
-
-      default: begin
-        // Something went wrong; go back to a known state.
-        next_state = IDLE;
       end
     endcase
   end
