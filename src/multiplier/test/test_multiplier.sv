ede8d4315baeeb79946979b400d72a6e3e522d70
Kier Davis <kierdavis@gmail.com>
2016-11-21 09:02:35 +0000

    Resolve naming conflict

--- src/multiplier/test/test_multiplier.sv
+++ src/multiplier/test/test_multiplier.sv
@@ -8,7 +8,7 @@ module test_multiplier;
   logic [N-1:0] multiplicand, multiplier;
   logic [N*2-1:0] product;
 
-  multiplier #(.N(N)) m (.*);
+  multiplier #(.N(N)) mult (.*);
 
   initial begin
     // Initialise inputs.
