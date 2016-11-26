1b6d088d78eb0490a255d39730819c0b320fb5cb
Kier Davis <kierdavis@gmail.com>
2016-11-26 02:42:09 +0000

    Do some more work on report

--- report/build.sh
+++ report/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+#pdflatex -halt-on-error -file-line-error -shell-escape -draftmode d1report.tex
 pdflatex -halt-on-error -file-line-error -shell-escape d1report.tex
--- report/shell.nix
+++ report/shell.nix
@@ -2,8 +2,24 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "d1-report-env";
   buildInputs = [
+    pkgs.python27Packages.pygments
     (pkgs.texlive.combine {
-      inherit (pkgs.texlive) scheme-basic graphics;
+      inherit (pkgs.texlive) scheme-basic
+        caption
+        etoolbox # dependency of minted
+        fancyvrb # dependency of minted
+        float # dependency of minted
+        framed # dependency of minted
+        ifplatform # dependency of minted
+        lineno # dependency of minted
+        minted
+        ms # dependency of todonotes
+        pgf # dependency of todonotes
+        todonotes
+        xcolor # dependency of minted, todonotes
+        xkeyval # dependency of todonotes
+        xstring # dependency of minted
+      ;
     })
   ];
-};
+}
