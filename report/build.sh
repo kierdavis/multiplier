d08b25f8de4dba32f60302da777b2033e3560d22
Kier Davis <kierdavis@gmail.com>
2016-11-27 17:07:03 +0000

    Work on report

--- report/build.sh
+++ report/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+set -eu
+pdflatex -halt-on-error -file-line-error -shell-escape -draftmode d1report.tex
+bibtex d1report
 pdflatex -halt-on-error -file-line-error -shell-escape -draftmode d1report.tex
 pdflatex -halt-on-error -file-line-error -shell-escape d1report.tex
--- report/shell.nix
+++ report/shell.nix
@@ -5,25 +5,29 @@ stdenv.mkDerivation {
     pkgs.python27Packages.pygments
     (pkgs.texlive.combine {
       inherit (pkgs.texlive) scheme-basic
+        biblatex
+        biblatex-ieee
         booktabs
         caption
-        etoolbox # dependency of minted
+        etoolbox # dependency of biblatex, minted
         fancyvrb # dependency of minted
         float # dependency of minted
         framed # dependency of minted
         ifplatform # dependency of minted
+        iftex # dependency of biblatex
         l3kernel # dependency of siunitx
         l3packages # dependency of siunitx
         lineno # dependency of minted
+        logreq # dependency of biblatex
         minted
         ms # dependency of todonotes
         pgf # dependency of todonotes
         siunitx
         todonotes
+        wrapfig
         xcolor # dependency of minted, todonotes
         xkeyval # dependency of todonotes
-        xstring # dependency of minted
-        wrapfig
+        xstring # dependency of biblatex, minted
       ;
     })
   ];
